72bc97b761358c60406482fd5dcd05e7a2eaf262
Viktor Gunin <guninada@yandex.ru>
2019-08-08 13:24:06 +0400

    Добавлен клик на кнопку

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-import hi from './js/banan';
+import { appendFile } from "fs";
 
-console.log('Привет2!');
-hi();
+    const popup = document.querySelector(".collector");
+    const buttonOpen = document.querySelector(".button-open");
+    const buttonClose = document.querySelector(".button-close");
+    
+    buttonOpen.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.remove("collector_hidden");
+    });
+    
+    buttonClose.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.add("collector_hidden");
+    });
+
+    document.addEventListener('keydown', function(evt) {
+        if(evt.keyCode===27) {
+          popup.classList.add('collector_hidden');
+        }
+        
+      });
+   
+      input.oninput = function() {
+        result.innerHTML = input.value;
+      };
